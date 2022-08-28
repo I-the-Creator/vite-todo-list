9d68de219ed4cadba42db306d00d45b2fee17e7c
I-the-Creator <andrey.burluckiy@gmail.com>
2022-08-28 18:49:36 +0200

    edit, delete and confirm buttons added to task component, logic for editing and deleting tasks implemented

--- src/views/components/InputTask/index.tsx
+++ src/views/components/InputTask/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
 
 //styles
 import styles from './index.module.scss'
@@ -20,5 +20,82 @@ export const InputTask: FC<IInputTaskProps> = ({
   onEdit,
   onRemove,
 }) => {
-  return <div className={styles.inputTask}>{title}</div>
+  const [checked, setChecked] = useState(false)
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [value, setValue] = useState(title) // default value for task editing - current title
+
+  const editTitleInputRef = useRef<HTMLInputElement>(null) // to bind input with clicked 'Edit' button to make input in focus
+
+  //on 'isEditMode' change
+  useEffect(() => {
+    if (isEditMode) {
+      editTitleInputRef?.current?.focus() // set focus attribute for input
+    }
+  }, [isEditMode])
+
+  return (
+    <div className={styles.inputTask}>
+      <label className={styles.inputTaskLabel}>
+        <input
+          type="checkbox"
+          checked={checked}
+          disabled={isEditMode}
+          className={styles.inputTaskCheckbox}
+          onChange={(e) => {
+            setChecked(e.target.checked)
+
+            // set delay for task deletion withafter checkbox clicked
+            if (e.target.checked) {
+              setTimeout(() => {
+                onDone(id) // on checked - delete task
+              }, 300)
+            }
+          }}
+        />
+        {isEditMode ? (
+          <input
+            value={value}
+            ref={editTitleInputRef} // connect 'ref' to input (input object)
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                onEdit(id, value) // update task on push Enter when editing task
+                setIsEditMode(false)
+              }
+            }}
+            className={styles.inputTaskEditTitle}
+          />
+        ) : (
+          <h3 className={styles.inputTaskTitle}>{title}</h3>
+        )}
+      </label>
+      {isEditMode ? (
+        <button
+          aria-label="Save"
+          className={styles.inputTaskSave}
+          onClick={() => {
+            onEdit(id, value) // update task on click
+            setIsEditMode(false)
+          }}
+        />
+      ) : (
+        <button
+          aria-label="Edit"
+          className={styles.inputTaskEdit}
+          onClick={() => {
+            setIsEditMode(true)
+          }}
+        />
+      )}
+      <button
+        aria-label="Remove"
+        className={styles.inputTaskRemove}
+        onClick={() => {
+          if (confirm('Are you sure?')) {
+            onRemove(id)
+          }
+        }}
+      />
+    </div>
+  )
 }
